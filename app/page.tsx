a59515982b50d8a74496638959f4312029e0be11
arifmithu <mdarifhossainmithu@gmail.com>
2025-05-02 11:14:04 +0600

    Update import font import

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,6 @@ export default function Page() {
             height={620}
             className="block md:hidden"
             alt="Screenshots of the dashboard project showing mobile version"
-            placeholder="blur"
           />
         </div>
       </div>
